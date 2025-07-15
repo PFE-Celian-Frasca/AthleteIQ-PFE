338d57e6d9d39dffffc07ac52f5e27d3d79779e6
Célian Frasca <celian.fras@gmail.com>
2025-07-15 19:17:17 +0200

    test: ajout de tests unitaires pour le provider de récupération des informations utilisateur

--- test/providers/groupe/group_actions/group_action_provider_test.dart
+++ test/providers/groupe/group_actions/group_action_provider_test.dart
@@ -4,6 +4,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mocktail/mocktail.dart';
 
 import 'package:athlete_iq/models/group/group_model.dart';
+import 'package:athlete_iq/models/user/user_model.dart';
 import 'package:athlete_iq/enums/enums.dart';
 import 'package:athlete_iq/providers/groupe/group_actions/group_action_provider.dart';
 import 'package:athlete_iq/providers/groupe/group_state.dart';
@@ -113,6 +114,144 @@ void main() {
 
       verify(() => mockNotif.showErrorToast(any(that: startsWith('Erreur lors de la création'))))
           .called(1);
+    });
+
+    test('updateGroup succès ⇒ appel service', () async {
+      when(() => mockGroupService.updateGroup(sampleGroup)).thenAnswer((_) async {});
+
+      final notifier = container.read(groupActionsProvider.notifier);
+      await notifier.updateGroup(sampleGroup, '123');
+
+      expect(container.read(groupActionsProvider), const GroupState.initial());
+      verify(() => mockGroupService.updateGroup(sampleGroup)).called(1);
+      verifyNever(() => mockNotif.showErrorToast(any()));
+    });
+
+    test('updateGroup erreur ⇒ state.error + toast', () async {
+      when(() => mockGroupService.updateGroup(sampleGroup)).thenThrow(Exception('fail'));
+
+      final notifier = container.read(groupActionsProvider.notifier);
+
+      await expectLater(() => notifier.updateGroup(sampleGroup, '123'), throwsA(isA<Exception>()));
+
+      container.read(groupActionsProvider).when(
+            error: (msg) => expect(msg, contains('fail')),
+            loading: () => fail('should not be loading'),
+            initial: () => fail('should not be initial'),
+            loaded: (_) => fail('should not reach loaded'),
+          );
+
+      verify(() => mockNotif.showErrorToast(any(that: contains('mise à jour')))).called(1);
+    });
+
+    test('leaveGroup succès ⇒ mise à jour membres', () async {
+      when(() => mockGroupService.updateGroup(any())).thenAnswer((_) async {});
+
+      final notifier = container.read(groupActionsProvider.notifier);
+      await notifier.leaveGroup(group: sampleGroup, currentUser: UserModel(
+        id: '123',
+        pseudo: 'p',
+        email: 'e',
+        createdAt: DateTime.now(),
+        sex: 'M',
+      ));
+
+      verify(() => mockGroupService.updateGroup(any())).called(1);
+      verifyNever(() => mockNotif.showErrorToast(any()));
+    });
+
+    test('leaveGroup erreur ⇒ state.error + toast', () async {
+      when(() => mockGroupService.updateGroup(any())).thenThrow(Exception('leave'));
+
+      final notifier = container.read(groupActionsProvider.notifier);
+
+      await expectLater(
+        () => notifier.leaveGroup(group: sampleGroup, currentUser: UserModel(
+          id: '123',
+          pseudo: 'p',
+          email: 'e',
+          createdAt: DateTime.now(),
+          sex: 'M',
+        )),
+        throwsA(isA<Exception>()),
+      );
+
+      verify(() => mockNotif.showErrorToast(any(that: contains('sortie')))).called(1);
+    });
+
+    test('joinGroup succès ⇒ mise à jour membres', () async {
+      when(() => mockGroupService.updateGroup(any())).thenAnswer((_) async {});
+
+      final notifier = container.read(groupActionsProvider.notifier);
+      await notifier.joinGroup(group: sampleGroup, currentUser: UserModel(
+        id: '456',
+        pseudo: 'x',
+        email: 'x@x.com',
+        createdAt: DateTime.now(),
+        sex: 'F',
+      ));
+
+      verify(() => mockGroupService.updateGroup(any())).called(1);
+      verifyNever(() => mockNotif.showErrorToast(any()));
+    });
+
+    test('joinGroup erreur ⇒ state.error + toast', () async {
+      when(() => mockGroupService.updateGroup(any())).thenThrow(Exception('join'));
+
+      final notifier = container.read(groupActionsProvider.notifier);
+
+      await expectLater(
+        () => notifier.joinGroup(group: sampleGroup, currentUser: UserModel(
+          id: '456',
+          pseudo: 'x',
+          email: 'x@x.com',
+          createdAt: DateTime.now(),
+          sex: 'F',
+        )),
+        throwsA(isA<Exception>()),
+      );
+
+      verify(() => mockNotif.showErrorToast(any(that: contains("ajout")))).called(1);
+    });
+
+    test('removeMemberFromGroup succès', () async {
+      when(() => mockGroupService.removeMemberFromGroup('g1', 'u2')).thenAnswer((_) async {});
+
+      final notifier = container.read(groupActionsProvider.notifier);
+      await notifier.removeMemberFromGroup('g1', 'u2');
+
+      verify(() => mockGroupService.removeMemberFromGroup('g1', 'u2')).called(1);
+      verifyNever(() => mockNotif.showErrorToast(any()));
+    });
+
+    test('removeMemberFromGroup erreur ⇒ state.error + toast', () async {
+      when(() => mockGroupService.removeMemberFromGroup('g1', 'u2')).thenThrow(Exception('remove'));
+
+      final notifier = container.read(groupActionsProvider.notifier);
+
+      await expectLater(() => notifier.removeMemberFromGroup('g1', 'u2'), throwsA(isA<Exception>()));
+
+      verify(() => mockNotif.showErrorToast(any(that: contains('suppression du membre')))).called(1);
+    });
+
+    test('deleteGroup succès ⇒ toast de confirmation', () async {
+      when(() => mockGroupService.deleteGroup('g1')).thenAnswer((_) async {});
+
+      final notifier = container.read(groupActionsProvider.notifier);
+      await notifier.deleteGroup('g1');
+
+      verify(() => mockGroupService.deleteGroup('g1')).called(1);
+      verify(() => mockNotif.showToast(any())).called(1);
+    });
+
+    test('deleteGroup erreur ⇒ state.error + toast', () async {
+      when(() => mockGroupService.deleteGroup('g1')).thenThrow(Exception('del'));
+
+      final notifier = container.read(groupActionsProvider.notifier);
+
+      await expectLater(() => notifier.deleteGroup('g1'), throwsA(isA<Exception>()));
+
+      verify(() => mockNotif.showErrorToast(any(that: contains('suppression du groupe')))).called(1);
     });
   });
 }
