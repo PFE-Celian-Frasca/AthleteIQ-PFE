7ee930abc53a592e578d6b18d29b2cae0f282857
CelianFrascaYnov <106919066+CelianFrascaYnov@users.noreply.github.com>
2025-08-04 11:30:40 +0200

    fix: corrige conversion m/s -> km/h (multiplication par 3.6) + test u… (#110)
    * fix: corrige conversion m/s -> km/h (multiplication par 3.6) + test unitaire
    
    * fix: ajout de classes MockTrace et MockHttpMetric pour les tests de performance
    
    * test: ajout de tests unitaires pour le suivi de performance et les classes MockTrace et MockHttpMetric
    
    ---------
    
    Co-authored-by: Célian Frasca <celian.fras@gmail.com>

--- lib/utils/performance_monitoring.dart
+++ lib/utils/performance_monitoring.dart
@@ -1,15 +1,106 @@
+import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_performance/firebase_performance.dart';
+import 'package:flutter/foundation.dart';
+
+class MockTrace implements Trace {
+  final Map<String, String> _attributes = {};
+  final Map<String, int> _metrics = {};
+
+  MockTrace(name);
+
+  @override
+  Future<void> start()  {
+    return Future.value();
+  }
+
+  @override
+  Future<void> stop()  {
+    return Future.value();
+  }
+
+  @override
+  void putAttribute(String name, String value) {
+    _attributes[name] = value;
+  }
+
+  @override
+  void removeAttribute(String name) {
+    _attributes.remove(name);
+  }
+
+  @override
+  void incrementMetric(String name, int value) {
+    _metrics[name] = (_metrics[name] ?? 0) + value;
+  }
+
+  @override
+  void setMetric(String name, int value) {
+    _metrics[name] = value;
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+class MockHttpMetric implements HttpMetric {
+
+  MockHttpMetric(url,httpMethod);
+
+  @override
+  Future<void> start()  {
+    return Future.value();
+  }
+
+  @override
+  Future<void> stop()  {
+    return Future.value();
+  }
+
+  @override
+  set httpResponseCode(int? httpResponseCode) {}
+
+  @override
+  set requestPayloadSize(int? requestPayloadSize) {}
+
+  @override
+  set responseContentType(String? responseContentType) {}
+
+  @override
+  set responsePayloadSize(int? responsePayloadSize) {}
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
 
 class PerformanceMonitoring {
   PerformanceMonitoring._();
 
+  static bool get isFirebaseInitialized {
+    try {
+      Firebase.app();
+      return true;
+    } on FirebaseException catch (_) {
+      return false;
+    }
+  }
+
   static Trace startTrace(String name) {
+    if (kDebugMode || !isFirebaseInitialized) {
+      return MockTrace(name);
+    }
+    
     final trace = FirebasePerformance.instance.newTrace(name);
     trace.start();
     return trace;
   }
 
   static HttpMetric startHttpMetric(String url, HttpMethod method) {
+
+    if (kDebugMode || !isFirebaseInitialized) {
+      return MockHttpMetric(url, method);
+    }
+    
     final metric = FirebasePerformance.instance.newHttpMetric(url, method);
     metric.start();
     return metric;
